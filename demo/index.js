17ef85445dc3d06686d7502a62c441ecfa8e8318
Michal Raczkowski <michal.raczkowski@zalando.de>
2016-01-22 11:56:17 +0100

    split index into css,js,html; attribute string values

---
+++ demo/index.js
@@ -0,0 +1,32 @@
+
+// register & configure elements
+
+// this component is configured in HTML via attributes
+var fatherSurname = new MyInput('#fatherSurname');
+
+// this component will be configured via properties - dynamically, all type of values possible
+var motherSurname = new MyInput('#motherSurname');
+motherSurname.label = "Mother Surname";
+motherSurname.clearLabel = "CLEAR";
+
+
+// set up external bindings
+// two-way data binding also possible here
+
+fatherSurname.host.addEventListener('clear', function() {
+    console.log('clear father surname');
+});
+
+motherSurname.host.addEventListener('clear', function() {
+    console.log('clear mother surname');
+});
+
+
+fatherSurname.host.addEventListener('changeText', function(ev) {
+    motherSurname.text = ev.detail.text;
+});
+
+motherSurname.host.addEventListener('changeText', function(ev) {
+    fatherSurname.text = ev.detail.text;
+});
+
--- src/my-input/my-input.js
+++ src/my-input/my-input.js
@@ -4,50 +4,71 @@
 
 function MyInput(selector) {
 
-    // component id
+    // PUBLIC STATIC - component id
     MyInput.id = MyInput.id || 0;
     MyInput.id++;
 
-    // HTML template
+    // PRIVATE STATIC - CSS class, HTML template
     var cssClass = "my-input";
     var labelId = cssClass + "-label-" + MyInput.id;
     var template = '<label for="' + labelId + '"></label><input id="' + labelId + '" type="text" /><button></button>';
 
-    // host element
+    // PUBLIC - host element
     this.host = document.querySelector(selector);
 
     // attach CSS class, render template
     this.host.classList.add(cssClass);
     this.host.innerHTML = template;
 
-    // elements
-    this.labelElem = this.host.querySelector('label');
-    this.buttonElem = this.host.querySelector('button');
-    this.inputElem = this.host.querySelector('input');
+    // PRIVATE - elements
+    this._labelElem = this.host.querySelector('label');
+    this._buttonElem = this.host.querySelector('button');
+    this._inputElem = this.host.querySelector('input');
 
     // register 'click' event
-    this.buttonElem.addEventListener('click', this.onClick.bind(this));
+    this._buttonElem.addEventListener('click', this.onButtonClick.bind(this));
 
     // register 'keyup' event
-    this.inputElem.addEventListener('keyup', this.onTextChange.bind(this));
+    this._inputElem.addEventListener('keyup', this.onTextChange.bind(this));
 
     // register custom event 'clear'
     var clearEventName = 'clear';
-    this.clearEvent = document.createEvent('CustomEvent');
-    this.clearEvent.initCustomEvent(clearEventName, true, true, null);
-    this.clearEvent.eventName = clearEventName;
+    this._clearEvent = document.createEvent('CustomEvent');
+    this._clearEvent.initCustomEvent(clearEventName, true, true, null);
+    this._clearEvent.eventName = clearEventName;
 
     // register custom event 'change'
     var changeEventName = 'changeText';
-    this.changeEvent = document.createEvent('CustomEvent');
-    this.changeEventData = {};
-    this.changeEvent.initCustomEvent(changeEventName, true, true, this.changeEventData);
-    this.changeEvent.eventName = changeEventName;
+    this._changeEvent = document.createEvent('CustomEvent');
+    this._changeEventData = {};
+    this._changeEvent.initCustomEvent(changeEventName, true, true, this._changeEventData);
+    this._changeEvent.eventName = changeEventName;
+
+    // read static attribute string values
+    var attrs = {};
+    attrs.label = this.host.getAttribute('label');
+    attrs.text = this.host.getAttribute('text');
+    attrs.clearLabel = this.host.getAttribute('clearLabel');
+
+    // initialize dynamic properties by static attribute string values
+    // delay via setTimeout - for all external bindings to work already
+    setTimeout(function() {
+        if(attrs.label) {
+            this.label = attrs.label;
+        }
+        if(attrs.text) {
+            this.text = attrs.text;
+            this.onTextChange();
+        }
+        if(attrs.clearLabel) {
+            this.clearLabel = attrs.clearLabel;
+        }
+    }.bind(this), 0);
 
 }
 
 
-// ----- methods -----
+// ----- PUBLIC - methods -----
 
 MyInput.prototype.clear = function() {
     // change text and fire change event
@@ -55,54 +76,54 @@ MyInput.prototype.clear = function() {
     this.onTextChange();
 
     // focus on input element
-    this.inputElem.focus();
+    this._inputElem.focus();
 
     // fire clear event
-    this.host.dispatchEvent(this.clearEvent);
+    this.host.dispatchEvent(this._clearEvent);
 };
 
 
-MyInput.prototype.onClick = function() {
+MyInput.prototype.onButtonClick = function() {
     this.clear();
 };
 
 
 MyInput.prototype.onTextChange = function() {
-    this.changeEventData.text = this.inputElem.value;
-    this.host.dispatchEvent(this.changeEvent);
+    this._changeEventData.text = this._inputElem.value;
+    this.host.dispatchEvent(this._changeEvent);
 };
 
 
 
 
-// ----- data binding -----
+// ----- PUBLIC - properties and data binding -----
 
 Object.defineProperty(MyInput.prototype, 'label', {
     get: function() {
-        return this.labelElem.textContent;
+        return this._labelElem.textContent;
     },
     set: function(label) {
-        this.labelElem.textContent = label;
+        this._labelElem.textContent = label;
     }
 });
 
 
 Object.defineProperty(MyInput.prototype, 'clearLabel', {
     get: function() {
-        return this.buttonElem.textContent;
+        return this._buttonElem.textContent;
     },
     set: function(clearLabel) {
-        this.buttonElem.textContent = clearLabel;
+        this._buttonElem.textContent = clearLabel;
     }
 });
 
 
 Object.defineProperty(MyInput.prototype, 'text', {
     get: function() {
-        return this.inputElem.value;
+        return this._inputElem.value;
     },
     set: function(text) {
-        this.inputElem.value = text;
+        this._inputElem.value = text;
     }
 });
 
